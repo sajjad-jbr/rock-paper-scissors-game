eeb7aa889b0efddca79f51a1f9b6dceb6bb5a04c
ui11 <ui11@asd.comk>
2022-11-03 11:47:41 +0330

    add footer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import Header from "./Header";
 import '../assets/styles/App.css';
 import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Main/>
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <h6>by sajjad </h6>
+        </div>
+    );
+};
+
+export default Footer;

6075166932a4185a9db5064556711a53a72b6186
sajjad-jbr <job.sajjad.jbr@gmail.com>
2022-11-03 11:15:17 +0330

    add images and header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div className='Header'>
+            <h1>Rock Paper Scissors</h1>
+        </div>
+    );
+}
+
+export default Header;

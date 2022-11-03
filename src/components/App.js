176f2d58f830075396fd2a11df057dbb888d6f02
ui11 <ui11@asd.comk>
2022-11-03 11:44:59 +0330

    add main component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import '../assets/styles/App.css';
+import Main from "./Main";
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Main/>
         </div>
     );
 }
